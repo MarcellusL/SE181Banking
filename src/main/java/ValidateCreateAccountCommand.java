fe11a4256dca98748c670b685e6db4249a6365e7
aj983 <aj983@drexel.edu>
2024-02-17 01:32:53 -0500

    - Implemented account creation validation. - Added in bounds for different accounts, so that it will not return false if checking or saving had an amount. - Checks to see if APR is within range 0-10. - Valid account id is 8 characters - CD account initial amount range is 1000-10000.

---
+++ src/main/java/ValidateCreateAccountCommand.java
@@ -0,0 +1,81 @@
+public class ValidateCreateAccountCommand {
+
+	private Bank bank;
+
+	public ValidateCreateAccountCommand(Bank bank) {
+		this.bank = bank;
+	}
+
+	public boolean validate(String[] commandWords) {
+		if (commandWords.length < 4) {
+			return false;
+		}
+
+		String accountType = commandWords[1].toLowerCase();
+
+		switch (accountType) {
+		case "checking":
+		case "savings":
+			return validateCheckingOrSavings(commandWords);
+
+		case "cd":
+			return validateCertificateDeposit(commandWords);
+
+		default:
+			return false;
+		}
+	}
+
+	private boolean validateCheckingOrSavings(String[] commandWords) {
+		if (commandWords.length != 4) {
+			return false;
+		}
+
+		String accountId = commandWords[2];
+		double apr;
+
+		try {
+			apr = Double.parseDouble(commandWords[3]);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+		return bank.getAccountUsingId(accountId) == null && isValidAccountId(accountId) && isValidAPR(apr);
+	}
+
+	private boolean validateCertificateDeposit(String[] commandWords) {
+		if (commandWords.length != 5) {
+			;
+			return false;
+		}
+		String accountId = commandWords[2];
+		double apr;
+		double initialAmount;
+
+		try {
+			apr = Double.parseDouble(commandWords[3]);
+			initialAmount = Double.parseDouble(commandWords[4]);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+		return bank.getAccountUsingId(accountId) == null && isValidAccountId(accountId) && isValidAPR(apr)
+				&& isValidCDAmount(initialAmount);
+	}
+
+	private boolean isValidAccountId(String accountId) {
+		return accountId.matches("\\d{8}");
+	}
+
+	private boolean isValidAPR(double apr) {
+		return apr >= 0 && apr <= 10;
+	}
+
+	private boolean isValidCDAmount(double initialAmount) {
+		return initialAmount >= 1000 && initialAmount <= 10000;
+	}
+
+	private boolean isValidAccountType(String accountType) {
+		return accountType.equals("checking") || accountType.equals("savings") || accountType.equals("cd");
+	}
+}
