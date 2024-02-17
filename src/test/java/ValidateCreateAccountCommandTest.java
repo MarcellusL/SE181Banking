965c8dfb6e989d4bc2a91929f198699e55dd38d3
aj983 <aj983@drexel.edu>
2024-02-17 02:05:18 -0500

    - Added test for invalid checking account with missing ID - Added test for invalid checking account with APR out of range - Added test for invalid savings account with missing APR - Added test for invalid checking account with APR below 0 - Added test for invalid CD account with initial balance below minimum @ $1000 - Added test for invalid checking account ID with more than 8 digits - Added test for invalid savings account ID with less than 8 digits

--- src/test/java/ValidateCreateAccountCommandTest.java
+++ src/test/java/ValidateCreateAccountCommandTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -14,21 +15,62 @@ public class ValidateCreateAccountCommandTest {
 	}
 
 	@Test
-	public void create_savings_account_command_is_valid() {
+	void valid_savings_account_command() {
 		String[] commandWords = { "createAccount", "savings", "12345678", "0.6" };
 		assertTrue(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
-	public void create_checking_account_is_valid() {
+	void valid_checking_account_command() {
 		String[] commandWords = { "createAccount", "checking", "22355679", "0.6" };
 		assertTrue(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
-	void create_certificate_deposits_account_is_valid() {
+	void valid_certificate_deposits_account_command() {
 		String[] commandWords = { "createAccount", "cd", "39504303", "0.4", "2000" };
 		assertTrue(createAccountValidator.validate(commandWords));
 	}
 
+	@Test
+	void invalid_checking_account_missing_account_id() {
+		String[] commandWords = { "createAccount", "savings", "", "2.0" };
+		assertFalse(createAccountValidator.validate(commandWords));
+	}
+
+	@Test
+	void invalid_checking_account_apr_out_of_range() {
+		String[] commandWords = { "createAccount", "checking", "15" };
+		assertFalse(createAccountValidator.validate(commandWords));
+	}
+
+	@Test
+	void invalid_savings_account_missing_apr() {
+		String[] commandWords = { "createAccount", "savings", "12345678" };
+		assertFalse(createAccountValidator.validate(commandWords));
+	}
+
+	@Test
+	void invalid_checking_account_apr_below_0() {
+		String[] commandWords = { "createAccount", "checking", "20496047", "-1.0" };
+		assertFalse(createAccountValidator.validate(commandWords));
+	}
+
+	@Test
+	void invalid_cd_account_initial_balance_below_1000() {
+		String[] commandWords = { "createAccount", "cd", "30549684", "5.6", "500" };
+		assertFalse(createAccountValidator.validate(commandWords));
+	}
+
+	@Test
+	void invalid_checking_account_id_more_than_8() {
+		String[] commandWords = { "createAccount", "checking", "120495023", "3.0" };
+		assertFalse(createAccountValidator.validate(commandWords));
+	}
+
+	@Test
+	void invalid_savings_account_id_less_than_8() {
+		String[] commandWords = { "createAccount", "savings", "12369" };
+		assertFalse(createAccountValidator.validate(commandWords));
+	}
 }
