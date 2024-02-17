ef04f985ed193f855e5c4d45f59fe8b14a904684
aj983 <aj983@drexel.edu>
2024-02-17 02:31:06 -0500

    - Refactored the name "createAccounts" to "create" as I ran into a problem in commandValidatorTest.

---
+++ src/main/java/CommandValidator.java
@@ -0,0 +1,28 @@
+public class CommandValidator {
+	private Bank bank;
+	private ValidateCreateAccountCommand validateCreateAccountCommand;
+	private ValidateDepositCommand validateDepositCommand;
+
+	public CommandValidator(Bank bank) {
+		validateCreateAccountCommand = new ValidateCreateAccountCommand(bank);
+		validateDepositCommand = new ValidateDepositCommand(bank);
+	}
+
+	public boolean validate(String command) {
+		String[] words = command.split(" ");
+		String action = words[0].toLowerCase();
+
+		return validateCommand(action, words);
+	}
+
+	public boolean validateCommand(String action, String[] words) {
+		switch (action) {
+		case "create":
+			return validateCreateAccountCommand.validate(words);
+
+		case "deposit":
+			return validateDepositCommand.validate(words);
+		}
+		return false;
+	}
+}
--- src/test/java/ValidateCreateAccountCommandTest.java
+++ src/test/java/ValidateCreateAccountCommandTest.java
@@ -16,61 +16,61 @@ public class ValidateCreateAccountCommandTest {
 
 	@Test
 	void valid_savings_account_command() {
-		String[] commandWords = { "createAccount", "savings", "12345678", "0.6" };
+		String[] commandWords = { "create", "savings", "12345678", "0.6" };
 		assertTrue(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
 	void valid_checking_account_command() {
-		String[] commandWords = { "createAccount", "checking", "22355679", "0.6" };
+		String[] commandWords = { "create", "checking", "22355679", "0.6" };
 		assertTrue(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
 	void valid_certificate_deposits_account_command() {
-		String[] commandWords = { "createAccount", "cd", "39504303", "0.4", "2000" };
+		String[] commandWords = { "create", "cd", "39504303", "0.4", "2000" };
 		assertTrue(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
 	void invalid_checking_account_missing_account_id() {
-		String[] commandWords = { "createAccount", "savings", "", "2.0" };
+		String[] commandWords = { "create", "savings", "", "2.0" };
 		assertFalse(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
 	void invalid_checking_account_apr_out_of_range() {
-		String[] commandWords = { "createAccount", "checking", "15" };
+		String[] commandWords = { "create", "checking", "15" };
 		assertFalse(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
 	void invalid_savings_account_missing_apr() {
-		String[] commandWords = { "createAccount", "savings", "12345678" };
+		String[] commandWords = { "create", "savings", "12345678" };
 		assertFalse(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
 	void invalid_checking_account_apr_below_0() {
-		String[] commandWords = { "createAccount", "checking", "20496047", "-1.0" };
+		String[] commandWords = { "create", "checking", "20496047", "-1.0" };
 		assertFalse(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
 	void invalid_cd_account_initial_balance_below_1000() {
-		String[] commandWords = { "createAccount", "cd", "30549684", "5.6", "500" };
+		String[] commandWords = { "create", "cd", "30549684", "5.6", "500" };
 		assertFalse(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
 	void invalid_checking_account_id_more_than_8() {
-		String[] commandWords = { "createAccount", "checking", "120495023", "3.0" };
+		String[] commandWords = { "create", "checking", "120495023", "3.0" };
 		assertFalse(createAccountValidator.validate(commandWords));
 	}
 
 	@Test
 	void invalid_savings_account_id_less_than_8() {
-		String[] commandWords = { "createAccount", "savings", "12369" };
+		String[] commandWords = { "create", "savings", "12369" };
 		assertFalse(createAccountValidator.validate(commandWords));
 	}
 	//
