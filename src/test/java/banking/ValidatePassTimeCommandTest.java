821c2eadbd056920b3fceb5091b461e59cb0f173
aj983 <aj983@drexel.edu>
2024-03-14 16:11:50 -0400

    - Created more test cases for ValidatePassTimeCommandTest in attempt to kill mutants.
    - Today I was in The Lawn at UCity Square.

--- src/test/java/banking/ValidatePassTimeCommandTest.java
+++ src/test/java/banking/ValidatePassTimeCommandTest.java
@@ -2,10 +2,15 @@ package banking;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ValidatePassTimeCommandTest {
+	private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
 	SavingsAccount savingsAccount;
 	CheckingAccount checkingAccount;
 	CertificateDeposit certificateDeposit;
@@ -19,68 +24,92 @@ public class ValidatePassTimeCommandTest {
 		savingsAccount = new SavingsAccount("87437100", 3.0);
 		certificateDeposit = new CertificateDeposit("98745632", 2.1, 2000);
 		validatePassTimeCommand = new ValidatePassTimeCommand(bank);
+		System.setOut(new PrintStream(outputStream));
 	}
 
 	@Test
 	void valid_pass_time_command() {
-		String[] commandWords = { "pass", "1" };
-		assertTrue(validatePassTimeCommand.validate(commandWords));
+		String[] commandParts = { "pass", "1" };
+		assertTrue(validatePassTimeCommand.validate(commandParts));
 	}
 
 	@Test
 	void invalid_pass_time_command_month_below_minimum() {
-		String[] commandWords = { "pass", "-10" };
-		assertFalse(validatePassTimeCommand.validate(commandWords));
+		String[] commandParts = { "pass", "-10" };
+		assertFalse(validatePassTimeCommand.validate(commandParts));
 	}
 
 	@Test
 	void invalid_pass_time_command_month_above_max() {
-		String[] commandWords = { "pass", "72" };
-		assertFalse(validatePassTimeCommand.validate(commandWords));
+		String[] commandParts = { "pass", "72" };
+		assertFalse(validatePassTimeCommand.validate(commandParts));
 	}
 
 	@Test
 	void invalid_pass_time_command_missing_parameter() {
-		String[] commandWords = { "", "1" };
-		assertFalse(validatePassTimeCommand.validate(commandWords));
+		String[] commandParts = { "", "1" };
+		assertFalse(validatePassTimeCommand.validate(commandParts));
+
+		String printedMessage = outputStream.toString().trim();
+		System.setOut(originalOut);
+		assertEquals("Invalid command. Usage: pass <months>", printedMessage);
 	}
 
 	@Test
 	void invalid_pass_time_command_missing_parameters() {
-		String[] commandWords = { "", "" };
-		assertFalse(validatePassTimeCommand.validate(commandWords));
+		String[] commandParts = { "", "" };
+		assertFalse(validatePassTimeCommand.validate(commandParts));
 	}
 
 	@Test
 	void invalid_pass_time_command_invalid_input_using_emoji() {
-		String[] commandWords = { "😂", "1" };
-		assertFalse(validatePassTimeCommand.validate(commandWords));
+		String[] commandParts = { "😂", "1" };
+		assertFalse(validatePassTimeCommand.validate(commandParts));
+
+		String printedMessage = outputStream.toString().trim();
+		System.setOut(originalOut);
+		assertEquals("Invalid command. Usage: pass <months>", printedMessage);
 	}
 
 	@Test
 	void invalid_pass_time_command_extra_parameter() {
-		String[] invalidMonths1 = { "pass", "0" };
-		assertFalse(validatePassTimeCommand.validate(invalidMonths1));
+		String[] commandParts = { "pass", "0" };
+		assertFalse(validatePassTimeCommand.validate(commandParts));
 	}
 
 	@Test
 	void invalid_pass_time_invalid_month() {
-		String[] invalidMonths2 = { "pass", "100" };
-		assertFalse(validatePassTimeCommand.validate(invalidMonths2));
+		String[] commandParts = { "pass", "100" };
+		assertFalse(validatePassTimeCommand.validate(commandParts));
+
+		String printedMessage = outputStream.toString().trim();
+		System.setOut(originalOut);
+		assertEquals("Invalid number of months. Please pass between 1 and 60 months.", printedMessage);
 	}
 
 	@Test
 	void invalid_pass_non_integer_for_months() {
-		String[] invalidMonths3 = { "pass", "invalidMonth" };
-		assertFalse(validatePassTimeCommand.validate(invalidMonths3));
+		String[] commandParts = { "pass", "invalidMonth" };
+		assertFalse(validatePassTimeCommand.validate(commandParts));
+
+		String printedMessage = outputStream.toString().trim();
+		System.setOut(originalOut);
+		assertEquals("Invalid number of months. Please pass between 1 and 60 months.", printedMessage);
 	}
 
 	@Test
 	void invalid_pass_time_command_bank_not_initialized() {
 		ValidatePassTimeCommand validatePassTimeCommand = new ValidatePassTimeCommand(null);
-		String[] commandWords = { "pass", "12" };
-		assertTrue(validatePassTimeCommand.validate(commandWords));
+		String[] commandParts = { "pass", "12" };
+		assertTrue(validatePassTimeCommand.validate(commandParts));
 		assertThrows(IllegalStateException.class, () -> validatePassTimeCommand.executePassTime(12));
+	}
+
+	@Test
+	void invalid_pass_time_month_is_float() {
+		String[] commandParts = { "pass", "12.5" };
+		assertFalse(validatePassTimeCommand.validate(commandParts));
+
 	}
 
 }
